6667df044268f624ff00fe06576899160d472f67
Kihong <kks4517@naver.com>
2024-05-12 15:06:38 +0900

    Problem011 재 풀이

--- src/doitcode/Problem011.java
+++ src/doitcode/Problem011.java
@@ -15,51 +15,43 @@ public class Problem011 {
   // 수열이 주어졌을때 주어진 수열을 만들 수 있는지 확인
   // 만들수 있다면 push와 pop을 수행해야 하는지 확인하는 프로그램을 작성
 
+  private static String solution(int[] array) {
+    StringBuilder sb = new StringBuilder();
+    Deque<Integer> deque = new ArrayDeque<>();
+    int num = 1;
+    for (int target : array) {
+      if (target >= num) {
+        while (target >= num) {
+          deque.addLast(num++);
+          sb.append("+").append("\n");
+        }
+        deque.removeLast();
+        sb.append("-").append("\n");
+      } else {
+        if (deque.removeLast() > target) {
+          return "NO";
+        }
+        sb.append("-").append("\n");
+      }
+    }
+
+    return sb.toString();
+  }
+
   public static void main(String[] args) {
     try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
-      int arraySize = Integer.parseInt(br.readLine());
-      int[] numbers = new int[arraySize];
-      for (int i = 0; i < arraySize; i++) {
-        numbers[i] = Integer.parseInt(br.readLine());
+      int size = Integer.parseInt(br.readLine());
+      int[] array = new int[size];
+      for (int i = 0; i < size; i++) {
+        array[i] = Integer.parseInt(br.readLine());
       }
-      String result = solution(numbers);
-
+      String result = solution(array);
       bw.write(result);
       bw.flush();
     } catch (IOException e) {
-      e.printStackTrace();
+      System.out.println(e.getStackTrace()[0]);
     }
-  }
-
-  private static String solution(int[] numbers) {
-    StringBuilder sb = new StringBuilder();
-    Deque<Integer> stack = new ArrayDeque<>();
-    int num = 1;
-    boolean result = true;
-    for (int target : numbers) {
-      if (target >= num) {
-        while (target >= num) {
-          stack.addLast(num++);
-          sb.append("+")
-              .append("\n");
-        }
-        stack.pollLast();
-        sb.append("-")
-            .append("\n");
-      } else {
-        if (stack.pollLast() > target) {
-          result = false;
-          break;
-        } else {
-          sb.append("-")
-              .append("\n");
-        }
-      }
-
-    }
-
-    return result ? sb.toString() : "NO";
   }
 
 }
